57759c16ccaaf31efa7d9e80d0191a52a76b1a53
Justin Biele <justin.biele@verys.com>
2019-05-18 14:05:24 -0700

    set up server with handlebars and serving public directory

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("file working!")
+const express = require("express")
+const exphbs = require("express-handlebars")
+const { join } = require("path")
+const server = express()
+
+// Serve static content for the app from the "public" directory in the application directory.
+server.use(express.static(join(__dirname, "public")))
+
+// Parse application body
+server.use(express.urlencoded({ extended: true }))
+server.use(express.json())
+
+server.engine("handlebars", exphbs({ defaultLayout: "main" }))
+server.set("view engine", "handlebars")
+
+server.listen(3000, () => {
+  console.log("server listening on port: 3000")
+})
